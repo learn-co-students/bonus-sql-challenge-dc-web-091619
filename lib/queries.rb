675e78d867da1bdf9f64d5c4534c3a9018be86e1
github username <github email address>
2019-09-29 11:13:25 -0400

    Done.

--- lib/queries.rb
+++ lib/queries.rb
@@ -1,5 +1,12 @@
 # Write methods that return SQL queries for each part of the challeng here
+# This dataset features a list of every guest Jon Stewart 
+# had as host of 'The Daily Show' and includes their name,
+# the date they appeared on the show, occupation, and category of data.
 
-def guest_with_most_appearances
-  # write your query here!
+class Guest
+
+  def guest_with_most_appearances
+    # write your query here!
+  end
+
 end
